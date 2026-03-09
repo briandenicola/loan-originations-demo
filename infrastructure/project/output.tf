faa4ff647bf036912f9a86133a827e84b3f80543
Brian Denicola <brian@briandenicola.com>
2026-03-09 13:02:04 -0500

    Removed Files

--- infrastructure/project/output.tf
+++ infrastructure/project/output.tf
@@ -17,3 +17,8 @@ output "PROJECT_ENDPOINT" {
     value = "https://${var.foundry_project.ai_foundry.name}.services.ai.azure.com/api/projects/${var.foundry_project.name}"
     sensitive = false
 }
+
+output "APPINSIGHTS_CONNECTION_STRING" {
+    value     = azurerm_application_insights.this.connection_string
+    sensitive = true
+}
